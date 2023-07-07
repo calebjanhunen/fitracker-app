b27c07d770a6108636c50046badd1a760279ca4c
Caleb Janhunen <caleb.janhunen@tulip.com>
2023-07-07 15:50:14 -0400

    feat: Add spacer component

---
+++ src/components/Spacer/Spacer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styled from 'styled-components';
+
+import { type theme } from '../../theme/theme';
+
+interface Props {
+    horizontal?: boolean;
+    size: keyof typeof theme.spacing;
+}
+
+const SpacingComponent = styled(View)<Props>`
+    width: ${(props) => (props.horizontal ? props.theme.spacing[props.size] : '0')};
+    height: ${(props) => (!props.horizontal ? props.theme.spacing[props.size] : '0')};
+`;
+
+export default function Spacer(props: Props): React.ReactElement {
+    console.log(props);
+    return <SpacingComponent horizontal={props.horizontal} size={props.size} />;
+}
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import PageView from './PageView/PageView';
+import Spacer from './Spacer/Spacer';
+import Text from './Text/Text';
+
+export { PageView, Spacer, Text };
