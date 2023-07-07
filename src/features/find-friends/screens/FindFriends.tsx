81c827816e0aacf0e48b219bfedae7f344281dd0
Caleb Janhunen <caleb.janhunen@tulip.com>
2023-07-06 21:23:52 -0400

    feat: Add main screens for each feature

---
+++ src/features/find-friends/screens/FindFriends.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function FindFriends() {
+    return (
+        <View>
+            <Text>FindFriends</Text>
+        </View>
+    );
+}
