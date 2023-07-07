9d4794d3f88963a65412bb4b653a2fe2c243f9e8
Caleb Janhunen <caleb.janhunen@tulip.com>
2023-07-07 15:58:15 -0400

    feat: Add text component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Text as DefaultText } from 'react-native';
+import styled from 'styled-components';
+import { type theme, type ThemeType } from '../../theme/theme';
+
+interface Props {
+    children: React.ReactNode;
+    variant: keyof typeof variants;
+    color?: keyof typeof theme.fontColors;
+}
+
+const title = (theme: ThemeType): string => `
+  font-size: ${theme.fontSize.title1};
+  font-family: ${theme.fonts.regular};
+`;
+
+const headline = (theme: ThemeType): string => `
+  font-size: ${theme.fontSize.headline};
+  font-family: ${theme.fonts.semibold};
+`;
+
+const subhead = (theme: ThemeType): string => `
+  font-size: ${theme.fontSize.subhead};
+  font-family: ${theme.fonts.regular};
+`;
+
+const body = (theme: ThemeType): string => `
+  font-size: ${theme.fontSize.body};
+  font-family: ${theme.fonts.regular};
+`;
+
+const variants = {
+    title,
+    headline,
+    subhead,
+    body,
+};
+
+const CustomText = styled(DefaultText)<{ variant: Props['variant']; color: Props['color'] }>`
+    ${({ variant, theme }) => variants[variant](theme)};
+    color: ${({ color, theme }) => (color ? theme.fontColors[color] : theme.fontColors.primary)};
+`;
+
+export default function Text(props: Props): React.ReactElement {
+    return (
+        <CustomText variant={props.variant} color={props.color}>
+            {props.children}
+        </CustomText>
+    );
+}
