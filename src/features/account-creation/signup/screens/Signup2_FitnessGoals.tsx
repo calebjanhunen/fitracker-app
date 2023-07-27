90877cba11f4453cd2c8f09fd76a915951d5488a
Caleb Janhunen <caleb.janhunen@tulip.com>
2023-07-27 16:02:01 -0400

    FIT-29: Create Select fitness goals page

---
+++ src/features/account-creation/signup/screens/Signup2_FitnessGoals.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function FitnessGoals(): React.ReactElement {
+    return (
+        <View>
+            <Text>FitnessGoals</Text>
+        </View>
+    );
+}
