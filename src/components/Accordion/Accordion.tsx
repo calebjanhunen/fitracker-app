3bdbc485c420a5d50aee149d1e47f9e48dbabef6
Caleb Janhunen <caleb.janhunen@tulip.com>
2023-07-31 11:23:28 -0400

    FIT-31: Add state variables for expanded accordion and selected skill level

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, type Dispatch, type SetStateAction } from 'react';
 import { TouchableOpacity, View } from 'react-native';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -7,14 +7,20 @@ import Spacer from '../Spacer/Spacer';
 import Text from '../Text/Text';
 
 interface Props {
+    id: number;
     title: string;
     text: string;
+    selectedSection: number;
+    setSelectedSection: Dispatch<SetStateAction<number>>;
+    expandedSection: number;
+    setExpandedSection: Dispatch<SetStateAction<number>>;
 }
 
 const AccordionHeaderContainer = styled(TouchableOpacity)`
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
+    padding: ${(props) => props.theme.spacing.xxs} 0;
 `;
 
 const AccordionTitle = styled(View)`
@@ -23,26 +29,56 @@ const AccordionTitle = styled(View)`
     gap: ${(props) => props.theme.spacing.xxs};
 `;
 
-export default function Accordion({ title, text }: Props): React.ReactElement {
-    const [toggleShowText, setToggleShowText] = useState<boolean>(false);
+const Line = styled(View)`
+    height: 1px;
+    background-color: ${(props) => props.theme.colors.grey};
+`;
 
+export default function Accordion({
+    title,
+    text,
+    id,
+    selectedSection,
+    setSelectedSection,
+    expandedSection,
+    setExpandedSection,
+}: Props): React.ReactElement {
     return (
         <View>
             <AccordionHeaderContainer
                 onPress={() => {
-                    setToggleShowText(!toggleShowText);
+                    setExpandedSection((prevExpandedSection) =>
+                        prevExpandedSection === id ? -1 : id
+                    );
                 }}
+                activeOpacity={1}
             >
                 <AccordionTitle>
-                    <Ionicons name='ellipse-outline' size={22} />
+                    <TouchableOpacity
+                        onPress={() => {
+                            setSelectedSection(id);
+                        }}
+                    >
+                        <Ionicons
+                            name={
+                                selectedSection === id
+                                    ? 'checkmark-circle-outline'
+                                    : 'ellipse-outline'
+                            }
+                            size={26}
+                        />
+                    </TouchableOpacity>
                     <Text variant='headline'>{title}</Text>
                 </AccordionTitle>
                 <Ionicons
-                    name={toggleShowText ? 'chevron-down-outline' : 'chevron-forward-outline'}
+                    name={
+                        expandedSection === id ? 'chevron-down-outline' : 'chevron-forward-outline'
+                    }
                     size={26}
                 />
             </AccordionHeaderContainer>
-            {toggleShowText && (
+            <Line />
+            {expandedSection === id && (
                 <View>
                     <Spacer size='xxs' />
                     <Text variant='body'>{text}</Text>
--- src/features/account-creation/signup/screens/Signup4_SkillLevel.tsx
+++ src/features/account-creation/signup/screens/Signup4_SkillLevel.tsx
@@ -1,31 +1,71 @@
 import React, { useState } from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { FlatList } from 'react-native';
 
 import { type StackScreenProps } from '@react-navigation/stack';
 
-import { Accordion, PageView, Spacer, Text } from '../../../../components';
+import { Accordion, Button, PageView, Spacer, Text } from '../../../../components';
 import { type RootStackParamList } from '../../../../navigation/AccountNavigation';
 import { SignupBody, SignupFooter } from '../components';
 
 type Props = StackScreenProps<RootStackParamList, 'SkillLevel'>;
 
 export default function SkillLevel({ navigation }: Props): React.ReactElement {
+    const [selectedLevel, setSelectedLevel] = useState<number>(-1);
+    const [expandedLevel, setExpendedLevel] = useState<number>(-1);
+
     return (
         <PageView>
             <Text variant='title' textAlign='center'>
                 Select Skill Level
             </Text>
             <SignupBody>
-                <Accordion
-                    title='Beginner'
-                    text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-                        laudantium tempore molestias eaque, excepturi quis deserunt illo iusto
-                        voluptatibus doloribus facere enim provident. Perferendis tempora, ratione
-                        vero obcaecati repellendus eos.'
+                <FlatList
+                    data={data}
+                    renderItem={({ item, index }) => (
+                        <Accordion
+                            title={item.title}
+                            text={item.text}
+                            id={index}
+                            selectedSection={selectedLevel}
+                            setSelectedSection={setSelectedLevel}
+                            expandedSection={expandedLevel}
+                            setExpandedSection={setExpendedLevel}
+                        />
+                    )}
+                    scrollEnabled={false}
                 />
-                <Text variant='headline'>Hello</Text>
+                <Button
+                    variant='full'
+                    backgroundColor='primary'
+                    textColor='white'
+                    onPress={() => {
+                        // navigation.push();
+                    }}
+                >
+                    Next
+                </Button>
+                <Spacer size='xl' />
             </SignupBody>
             <SignupFooter navigation={navigation} />
         </PageView>
     );
 }
+
+const data = [
+    {
+        title: 'New',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendislaudantium tempore molestias eaque excepturi quis deserunt illo iusto voluptatibus doloribus facere enim provident. Perferendis tempora, ratione vero obcaecati repellendus eos.',
+    },
+    {
+        title: 'Beginner',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendislaudantium tempore molestias eaque excepturi quis deserunt illo iusto voluptatibus doloribus facere enim provident. Perferendis tempora, ratione vero obcaecati repellendus eos.',
+    },
+    {
+        title: 'Intermediate',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendislaudantium tempore molestias eaque excepturi quis deserunt illo iusto voluptatibus doloribus facere enim provident. Perferendis tempora, ratione vero obcaecati repellendus eos.',
+    },
+    {
+        title: 'Expert',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendislaudantium tempore molestias eaque excepturi quis deserunt illo iusto voluptatibus doloribus facere enim provident. Perferendis tempora, ratione vero obcaecati repellendus eos.',
+    },
+];
