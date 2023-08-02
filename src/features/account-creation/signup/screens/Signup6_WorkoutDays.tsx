566d63443ce6d46474953af11a138b3a4d3dfa18
Caleb Janhunen <caleb.janhunen@tulip.com>
2023-08-01 22:50:18 -0400

    FIT-41: Add signup context to workout days page

--- src/features/account-creation/signup/screens/Signup6_WorkoutDays.tsx
+++ src/features/account-creation/signup/screens/Signup6_WorkoutDays.tsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { FlatList } from 'react-native';
 
 import { type StackScreenProps } from '@react-navigation/stack';
 
 import { Button, PageView, Spacer, Text } from '../../../../components';
 import { type RootStackParamList } from '../../../../navigation/AccountNavigation';
+import { SignupDataContext } from '../../../../services/signup/SignupDataContext';
+import { SignupActionTypes } from '../../../../services/signup/SignupDataReducer';
 import { SignupBody, SignupFooter, WorkoutDayOrTimeBtn } from '../components';
 
 type Props = StackScreenProps<RootStackParamList, 'WorkoutDays'>;
 
 export default function WorkoutDays({ navigation }: Props): React.ReactElement {
-    const [selectedDays, setSelectedDays] = useState<number[]>([]);
-
+    const { signupData, dispatchSignupData } = useContext(SignupDataContext);
+    const [selectedDayIds, setSelectedDayIds] = useState<number[]>(
+        signupData.workoutDays
+            ? signupData.workoutDays.map((day) =>
+                  workoutDays.findIndex((workoutDay) => workoutDay.day === day)
+              )
+            : []
+    );
+    const isNextBtnDisabled = selectedDayIds.length === 0;
     return (
         <PageView>
             <Text variant='title' textAlign='center'>
@@ -24,8 +33,8 @@ export default function WorkoutDays({ navigation }: Props): React.ReactElement {
                         <WorkoutDayOrTimeBtn
                             id={index}
                             text={item.day}
-                            selectedIds={selectedDays}
-                            setSelectedIds={setSelectedDays}
+                            selectedIds={selectedDayIds}
+                            setSelectedIds={setSelectedDayIds}
                         />
                     )}
                     ItemSeparatorComponent={() => <Spacer size='md' />}
@@ -35,7 +44,13 @@ export default function WorkoutDays({ navigation }: Props): React.ReactElement {
                 variant='full'
                 backgroundColor='primary'
                 textColor='white'
+                disabled={isNextBtnDisabled}
                 onPress={() => {
+                    const selectedDays = selectedDayIds.map((id) => workoutDays[id].day);
+                    dispatchSignupData({
+                        type: SignupActionTypes.ADD_WORKOUT_DAYS,
+                        payload: selectedDays,
+                    });
                     navigation.push('WorkoutTimes');
                 }}
             >
