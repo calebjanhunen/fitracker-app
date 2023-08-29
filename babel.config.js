b8f95b8fc638831aca1f9860a85023f886848a60
Caleb Janhunen <caleb.janhunen@tulip.com>
2023-08-29 12:05:10 -0400

    Fix dependency errors

--- babel.config.js
+++ babel.config.js
@@ -11,12 +11,7 @@ module.exports = function (api) {
                     path: '.env',
                 },
             ],
-            [
-                'react-native-reanimated/plugin',
-                {
-                    relativeSourceLocation: true,
-                },
-            ],
+            'react-native-reanimated/plugin',
         ],
     };
 };
