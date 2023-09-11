88e0eaaf5c0049cc05f1dc076b9ea703c9d917a9
Caleb Janhunen <caleb.janhunen@tulip.com>
2023-09-11 08:56:07 -0400

    FIT-77: Install react-query

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 
-import { PortalProvider } from '@gorhom/portal';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { MenuProvider } from 'react-native-popup-menu';
-import { ThemeProvider } from 'styled-components';
-
 import {
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
     useFonts,
 } from '@expo-google-fonts/inter';
+import { PortalProvider } from '@gorhom/portal';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { MenuProvider } from 'react-native-popup-menu';
+import { ThemeProvider } from 'styled-components';
+
 import BaseNavigation from './src/navigation';
 import { AuthProvider } from './src/services/context/AuthContext';
 import { theme } from './src/theme/theme';
+
+const queryClient = new QueryClient();
 
 export default function App(): React.ReactElement | null {
     const [fontLoaded] = useFonts({
@@ -29,17 +32,19 @@ export default function App(): React.ReactElement | null {
 
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
-            <ThemeProvider theme={theme}>
-                <AuthProvider>
-                    <MenuProvider>
-                        <PortalProvider>
-                            <SafeAreaView style={{ flex: 1 }}>
-                                <BaseNavigation />
-                            </SafeAreaView>
-                        </PortalProvider>
-                    </MenuProvider>
-                </AuthProvider>
-            </ThemeProvider>
+            <QueryClientProvider client={queryClient}>
+                <ThemeProvider theme={theme}>
+                    <AuthProvider>
+                        <MenuProvider>
+                            <PortalProvider>
+                                <SafeAreaView style={{ flex: 1 }}>
+                                    <BaseNavigation />
+                                </SafeAreaView>
+                            </PortalProvider>
+                        </MenuProvider>
+                    </AuthProvider>
+                </ThemeProvider>
+            </QueryClientProvider>
         </GestureHandlerRootView>
     );
 }
