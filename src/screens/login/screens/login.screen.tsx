7294a31c3cfc5109b19b1246727da2f9535a9c99
Caleb Janhunen <calebjanhunen@gmail.com>
2024-05-21 23:43:00 -0400

    Add login form

--- src/screens/login/screens/login.screen.tsx
+++ src/screens/login/screens/login.screen.tsx
@@ -1,11 +1,42 @@
-import { Input, Layout } from '@ui-kitten/components';
+import { Button, Input, Layout, Text } from '@ui-kitten/components';
+import { TouchableWithoutFeedback } from '@ui-kitten/components/devsupport';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Keyboard, StyleSheet, View } from 'react-native';
+import { Spacer } from 'src/components';
 
 export default function LoginScreen(): React.ReactElement {
     return (
-        <Layout>
-            <Input></Input>
-        </Layout>
+        <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss} accessible={false}>
+            <Layout style={loginStyles.layout}>
+                <Spacer size='spacing-8' />
+                <Spacer size='spacing-8' />
+                <Spacer size='spacing-8' />
+                <Spacer size='spacing-8' />
+                <Spacer size='spacing-8' />
+                <Spacer size='spacing-8' />
+                <Spacer size='spacing-8' />
+                <Text category='h1' status='primary'>
+                    Fitracker
+                </Text>
+                <Spacer size='spacing-8' />
+                <View style={loginStyles.loginForm}>
+                    <Input placeholder='Username' />
+                    <Input placeholder='Password' />
+                    <Button>Login</Button>
+                </View>
+            </Layout>
+        </TouchableWithoutFeedback>
     );
 }
+
+const loginStyles = StyleSheet.create({
+    layout: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    loginForm: {
+        gap: 16,
+        flex: 1,
+        alignItems: 'center',
+    },
+});
