ff2363b08dd38ee1b8faf6184ae78ec59be711ba
Caleb Janhunen <calebjanhunen@gmail.com>
2024-05-22 20:58:24 -0400

    Add logout button

--- src/screens/profile/profile-screen.tsx
+++ src/screens/profile/profile-screen.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+
+import { Button, Layout } from '@ui-kitten/components';
+import { useAuth } from 'src/hooks/useAuth';
 
 export default function ProfileScreen(): React.ReactElement {
+    const { logout } = useAuth();
+
+    function handleLogout(): void {
+        void logout();
+    }
+
     return (
-        <View>
-            <Text>ProfileScreen</Text>
-        </View>
+        <Layout style={{ flex: 1 }}>
+            <Button onPress={handleLogout}>Logout</Button>
+        </Layout>
     );
 }
