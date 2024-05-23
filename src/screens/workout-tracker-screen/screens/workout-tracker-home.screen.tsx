49673361a51ae0533b99a797b736deae87edeabc
Caleb Janhunen <calebjanhunen@gmail.com>
2024-05-23 14:18:49 -0400

    Display proper ui based on error, loading, data

--- src/screens/workout-tracker-screen/screens/workout-tracker-home.screen.tsx
+++ src/screens/workout-tracker-screen/screens/workout-tracker-home.screen.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-import { Button, Layout, List, Text } from '@ui-kitten/components';
+import { Button, Layout, List, Spinner, Text } from '@ui-kitten/components';
 
+import { StyleSheet, View } from 'react-native';
 import { Spacer } from 'src/components';
 import { useWorkouts } from 'src/hooks/useWorkouts';
 import { type Workout } from 'src/interfaces/workout';
 import WorkoutHistoryCard from 'src/screens/workout-tracker-screen/components/workout-history-card';
 
 export default function WorkoutTrackerHomeScreen(): React.ReactElement {
-    const { workouts } = useWorkouts();
+    const { workouts, isLoading, error } = useWorkouts();
 
     const renderWorkoutHistoryCard = ({ item }: { item: Workout }): React.ReactElement => (
         <WorkoutHistoryCard workout={item} />
@@ -20,12 +21,30 @@ export default function WorkoutTrackerHomeScreen(): React.ReactElement {
             <Button>Start Empty Workout</Button>
             <Spacer size='spacing-5' />
             <Text category='h4'>Workout History:</Text>
-            <List
-                data={workouts}
-                renderItem={renderWorkoutHistoryCard}
-                ItemSeparatorComponent={() => <Spacer size='spacing-4' />}
-                style={{ backgroundColor: 'transparent' }}
-            />
+            <View style={styles.workoutHistoryContainer}>
+                {isLoading ? (
+                    <Spinner size='giant' />
+                ) : error ? (
+                    <Text status='danger'>{error}</Text>
+                ) : workouts ? (
+                    <List
+                        data={workouts}
+                        renderItem={renderWorkoutHistoryCard}
+                        ItemSeparatorComponent={() => <Spacer size='spacing-4' />}
+                        style={{ backgroundColor: 'transparent' }}
+                    />
+                ) : (
+                    <Text>No Workouts to display</Text>
+                )}
+            </View>
         </Layout>
     );
 }
+
+const styles = StyleSheet.create({
+    workoutHistoryContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
