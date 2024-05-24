842b693a057f34a5c8dab58da7424c7231bc32fa
Caleb Janhunen <calebjanhunen@gmail.com>
2024-05-23 20:37:36 -0400

    Add list of exercises

--- src/screens/workout-tracker-screen/screens/workout-tracker-form.screen.tsx
+++ src/screens/workout-tracker-screen/screens/workout-tracker-form.screen.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 
-import { Button, Input } from '@ui-kitten/components';
+import { Button, Input, List } from '@ui-kitten/components';
 
+import { View } from 'react-native';
 import { PageView, Spacer } from 'src/components';
 import Exercise from '../components/workout-tracker-form/exercise';
 
 export default function WorkoutTrackerFormScreen(): React.ReactElement {
+    const renderExercise = (): React.ReactElement => <Exercise />;
+
     return (
         <PageView>
             <Input placeholder='Enter Workout Name' />
             <Spacer size='spacing-8' />
-            <Exercise />
+            <View>
+                <List data={[0, 0]} renderItem={renderExercise} />
+            </View>
             <Spacer size='spacing-8' />
             <Button size='small'>Add Exercise</Button>
         </PageView>
