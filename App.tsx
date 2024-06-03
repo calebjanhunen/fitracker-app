4b7806c814cfcf111b0a22492004ef66a68e9ca7
Caleb Janhunen <calebjanhunen@gmail.com>
2024-06-02 22:48:38 -0400

    Initialize react-query

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import * as eva from '@eva-design/eva';
 import '@expo/metro-runtime';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ApplicationProvider } from '@ui-kitten/components';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { MenuProvider } from 'react-native-popup-menu';
 
 import { StatusBar } from 'react-native';
+import { queryClient } from 'src/api/config/react-query.config';
 import BaseNavigation from 'src/navigation/base-navigation';
 import { AuthProvider } from 'src/state/context/auth-context';
 import { WorkoutFormProvider } from 'src/state/context/workout-form-context';
@@ -14,19 +16,24 @@ import { WorkoutInProgressProvider } from 'src/state/context/workout-in-progress
 
 export default function App(): React.ReactElement | null {
     return (
-        <ApplicationProvider {...eva} theme={eva.light}>
-            <GestureHandlerRootView style={{ flex: 1 }}>
-                <AuthProvider>
-                    <WorkoutFormProvider>
-                        <WorkoutInProgressProvider>
-                            <MenuProvider>
-                                <BaseNavigation />
-                                <StatusBar barStyle='dark-content' backgroundColor='transparent' />
-                            </MenuProvider>
-                        </WorkoutInProgressProvider>
-                    </WorkoutFormProvider>
-                </AuthProvider>
-            </GestureHandlerRootView>
-        </ApplicationProvider>
+        <QueryClientProvider client={queryClient}>
+            <ApplicationProvider {...eva} theme={eva.light}>
+                <GestureHandlerRootView style={{ flex: 1 }}>
+                    <AuthProvider>
+                        <WorkoutFormProvider>
+                            <WorkoutInProgressProvider>
+                                <MenuProvider>
+                                    <BaseNavigation />
+                                    <StatusBar
+                                        barStyle='dark-content'
+                                        backgroundColor='transparent'
+                                    />
+                                </MenuProvider>
+                            </WorkoutInProgressProvider>
+                        </WorkoutFormProvider>
+                    </AuthProvider>
+                </GestureHandlerRootView>
+            </ApplicationProvider>
+        </QueryClientProvider>
     );
 }
