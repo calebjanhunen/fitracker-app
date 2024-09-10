0a7f197ef11b876c2d63823d649e775bc5ee22ed
Caleb Janhunen <calebjanhunen@gmail.com>
2024-09-09 21:37:54 -0400

    Add basic home page

--- src/app/(app)/(workout-tracker)/home.tsx
+++ src/app/(app)/(workout-tracker)/home.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useTheme } from 'tamagui';
+import { Button, useTheme } from 'tamagui';
 
 export default function Home() {
     const theme = useTheme();
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: theme.background.get() }}>
-            <Text>Hello</Text>
+        <SafeAreaView
+            style={{ flex: 1, backgroundColor: theme.background.val, paddingHorizontal: 16 }}
+        >
+            <Button fontWeight='bold' backgroundColor='$color.green8Light' color='white'>
+                Start Workout
+            </Button>
         </SafeAreaView>
     );
 }
