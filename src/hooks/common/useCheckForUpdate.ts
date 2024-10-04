94c5df0603b4b1cb374858e42b2fc9d5b7cbde04
Caleb Janhunen <34351243+calebjanhunen@users.noreply.github.com>
2024-10-04 13:16:31 -0400

    Add update notifier (#96)
    * Add update check
    
    * Change configuration based on build
    
    * Add preview
    
    * Fix update notifier

---
+++ src/hooks/common/useCheckForUpdate.ts
@@ -0,0 +1,34 @@
+import Constants from 'expo-constants';
+import * as Updates from 'expo-updates';
+import { useEffect } from 'react';
+import { Alert } from 'react-native';
+
+export function useCheckForUpdate() {
+    useEffect(() => {
+        void checkForUpdates();
+    }, []);
+
+    async function checkForUpdates() {
+        if (Constants.expoConfig?.extra?.ENVIRONMENT === 'development') {
+            return;
+        }
+
+        try {
+            const update = await Updates.checkForUpdateAsync();
+            if (update.isAvailable) {
+                await Updates.fetchUpdateAsync();
+                // Inform the user that the update has been downloaded and will be applied on the next app restart
+                Alert.alert(
+                    'Update Available',
+                    'A new update was downloaded and will be applied when you restart the app.',
+                    [
+                        { text: 'Restart Now', onPress: async () => await Updates.reloadAsync() },
+                        { text: 'Later' },
+                    ]
+                );
+            }
+        } catch (e) {
+            Alert.alert('Could not update.', 'Error encountered when trying to update:');
+        }
+    }
+}
