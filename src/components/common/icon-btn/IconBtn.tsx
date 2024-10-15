8e63e2d5fa807a26655a343b5ea41c7528a415bd
Caleb Janhunen <calebjanhunen@gmail.com>
2024-10-15 16:54:52 -0400

    Update IconBtn component with colors

--- src/components/common/icon-btn/IconBtn.tsx
+++ src/components/common/icon-btn/IconBtn.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import IonIcons from '@expo/vector-icons/Ionicons';
-import { Button } from 'tamagui';
+import { Button, Spinner, useTheme } from 'tamagui';
 
 interface Props {
     onPress: () => void;
     icon: keyof typeof IonIcons.glyphMap;
+    backgroundColor?: keyof ReturnType<typeof useTheme>;
+    iconColor?: keyof ReturnType<typeof useTheme>;
+    isLoading?: boolean;
 }
 
-export default function IconBtn({ onPress, icon }: Props) {
+export default function IconBtn({ onPress, icon, backgroundColor, iconColor, isLoading }: Props) {
     return (
         <Button
             fontWeight='bold'
@@ -16,8 +19,9 @@ export default function IconBtn({ onPress, icon }: Props) {
             paddingVertical='$1'
             height='0'
             onPress={onPress}
+            backgroundColor={backgroundColor}
         >
-            <IonIcons name={icon} size={24} />
+            {isLoading ? <Spinner /> : <IonIcons name={icon} size={24} color={iconColor} />}
         </Button>
     );
 }
