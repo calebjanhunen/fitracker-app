a3b98d88b6005271f2c1e18dbcab09e3fec864b6
Caleb Janhunen <calebjanhunen@gmail.com>
2024-10-15 20:38:37 -0400

    Add index for utils

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+export { formatDate } from './FormatDate';
+export { formatStopwatchTime } from './FormatStopwatchTime';
+export { formatTime } from './FormatTime';
+export { formatWorkoutDuration } from './formatWorkoutDuration';
+export { getOrdinalSuffix } from './GetOrdinalSuffix';
