7cae0b01403f77e27617ed428046ea09403e5f01
Caleb Janhunen <34351243+calebjanhunen@users.noreply.github.com>
2024-10-16 23:30:49 -0400

    Disable xp calculation in production (#115)
    * Display under construction text if not in development environment
    
    * Bump version

--- src/app/(app)/workout-tracker/PostWorkoutSummary.tsx
+++ src/app/(app)/workout-tracker/PostWorkoutSummary.tsx
@@ -1,8 +1,8 @@
+import Constants from 'expo-constants';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button, H2, H4, H6, Separator, View, XStack, YStack } from 'tamagui';
-
+import { Button, H2, H4, H6, Separator, SizableText, View, XStack, YStack } from 'tamagui';
 export default function PostWorkoutSummary() {
     const router = useRouter();
     const { xpGainedFromWeeklyGoal, totalGainedXp } = useLocalSearchParams<{
@@ -24,20 +24,44 @@ export default function PostWorkoutSummary() {
         >
             <View alignItems='center' justifyContent='center' flex={1}>
                 <H2 paddingBottom='$space.3'>Workout Completed!</H2>
-                <H4>XP gained from workout:</H4>
-                <XStack gap='$space.3'>
-                    <YStack alignItems='flex-end'>
-                        <H6>Weekly Goal: </H6>
-                        <Separator borderWidth='1' alignSelf='stretch' borderColor='$blue10' />
-                        <H6>Total XP Gained: </H6>
-                    </YStack>
-                    <YStack alignItems='flex-end'>
-                        <H6>{xpGainedFromWeeklyGoal}</H6>
-                        <Separator borderWidth='1' alignSelf='stretch' borderColor='$blue10' />
+                {Constants.expoConfig?.extra?.ENVIRONMENT !== 'development' ? (
+                    <>
+                        <SizableText color='$orange10'>
+                            ====================================
+                        </SizableText>
+                        <SizableText color='$orange10' fontWeight='bold'>
+                            Workout XP calulation is under construction
+                        </SizableText>
+                        <SizableText color='$orange10'>
+                            ====================================
+                        </SizableText>
+                    </>
+                ) : (
+                    <>
+                        <H4>XP gained from workout:</H4>
+                        <XStack gap='$space.3'>
+                            <YStack alignItems='flex-end'>
+                                <H6>Weekly Goal: </H6>
+                                <Separator
+                                    borderWidth='1'
+                                    alignSelf='stretch'
+                                    borderColor='$blue10'
+                                />
+                                <H6>Total XP Gained: </H6>
+                            </YStack>
+                            <YStack alignItems='flex-end'>
+                                <H6>{xpGainedFromWeeklyGoal}</H6>
+                                <Separator
+                                    borderWidth='1'
+                                    alignSelf='stretch'
+                                    borderColor='$blue10'
+                                />
 
-                        <H6>{totalGainedXp}</H6>
-                    </YStack>
-                </XStack>
+                                <H6>{totalGainedXp}</H6>
+                            </YStack>
+                        </XStack>
+                    </>
+                )}
             </View>
             <Button backgroundColor='$blue6' color='$blue10' fontWeight='bold' onPress={onBtnPress}>
                 Return to Home
