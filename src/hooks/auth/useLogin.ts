49e0855157a990f84b73153ccc3f79c521f2d56a
Caleb Janhunen <calebjanhunen@gmail.com>
2024-11-13 23:05:22 -0500

    Set user in login success

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,9 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'expo-router';
+import { useDispatch } from 'react-redux';
+import { ILoginResponse } from 'src/api/auth-service/interfaces/login-response';
 import { LoginRequestDto } from 'src/api/auth-service/interfaces/requests/login-request-dto';
 import * as AuthApi from 'src/api/auth-service/login-service';
 import { IErrorResponse } from 'src/api/client';
 import { useAuth } from 'src/context/auth-context/AuthContext';
+import { setUser } from 'src/redux/user/UserSlice';
 
 interface IUseLogin {
     isLoading: boolean;
@@ -13,17 +16,19 @@ interface IUseLogin {
 
 export function useLogin(): IUseLogin {
     const router = useRouter();
+    const dispatch = useDispatch();
     const { setAccessToken } = useAuth();
 
     const {
         mutate: login,
         isPending: isLoading,
         error,
-    } = useMutation<string, IErrorResponse, LoginRequestDto>({
+    } = useMutation<ILoginResponse, IErrorResponse, LoginRequestDto>({
         mutationFn: AuthApi.login,
-        onSuccess: (accessToken) => {
-            setAccessToken(accessToken);
-            router.replace('/(app)/workout-tracker');
+        onSuccess: (response) => {
+            dispatch(setUser(response.user));
+            router.replace('/(workout-tracker');
+            setAccessToken(response.accessToken);
         },
     });
 
