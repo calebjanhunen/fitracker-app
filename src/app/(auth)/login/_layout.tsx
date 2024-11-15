99a0240be263343ec90d539155488132f6442e29
Caleb Janhunen <calebjanhunen@gmail.com>
2024-11-14 20:38:57 -0500

    Implement resetting password

--- src/app/(auth)/login/_layout.tsx
+++ src/app/(auth)/login/_layout.tsx
@@ -4,12 +4,13 @@ import React from 'react';
 export default function LoginLayout() {
     return (
         <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name='index' />
+            <Stack.Screen name='index' options={{ gestureEnabled: false }} />
             <Stack.Screen name='VerifyEmail' options={{ gestureEnabled: false }} />
             <Stack.Screen
                 name='ForgotPassword'
                 options={{ headerBackTitleVisible: false, headerShown: true, headerTitle: '' }}
             />
+            <Stack.Screen name='ResetPassword' />
         </Stack>
     );
 }
