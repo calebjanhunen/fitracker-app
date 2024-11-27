16d1130420e7e91ef68b0a3dc526445f6513676d
Caleb Janhunen <calebjanhunen@gmail.com>
2024-11-26 22:41:19 -0500

    Add lastUpdatedAt to workout form

--- src/redux/workout-form/WorkoutFormMiddleware.ts
+++ src/redux/workout-form/WorkoutFormMiddleware.ts
@@ -9,6 +9,12 @@ export const saveWorkoutMiddleware: Middleware = (storeAPI) => (next) => async (
     if (action.type.startsWith('workoutForm/')) {
         // calling this after next(action) ensures that it's the updated state
         const workoutState = storeAPI.getState().workoutForm;
+
+        if (action.type !== 'workoutForm/updateLastUpdatedAt') {
+            storeAPI.dispatch({
+                type: 'workoutForm/updateLastUpdatedAt',
+            });
+        }
         try {
             // Convert the state to a string and store it in AsyncStorage
             await AsyncStorage.setItem(LocalStorageKeys.workoutForm, JSON.stringify(workoutState));
