0e3bb70cd76b32a19b762796a0dd78e9361359e1
Caleb Janhunen <calebjanhunen@gmail.com>
2024-12-28 23:34:07 -0500

    Use generated code for creating workout template

--- src/api/hooks/useWorkoutTemplates.ts
+++ src/api/hooks/useWorkoutTemplates.ts
@@ -1,6 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
-import { apiClient } from '../client';
-import { WorkoutTemplatesApi } from '../generated';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { IWorkoutTemplateFormState } from 'src/redux/workout-template-form/IWorkoutTemplateForm';
+import { apiClient, IErrorResponse } from '../client';
+import { WorkoutTemplateRequestDto, WorkoutTemplatesApi } from '../generated';
+import { queryClient } from '../react-query-client';
 
 export const WorkoutTemplateQueryKeys = {
     getAllWorkoutTemplates: ['workoutTemplates'],
@@ -18,4 +20,48 @@ export function useGetAllWorkoutTemplates() {
     });
 
     return { data: data ?? [], isLoading, error };
+}
+
+export function useCreateWorkoutTemplate(
+    onSuccessCallback: () => void,
+    onErrorCallback: (error: IErrorResponse) => void
+) {
+    const {
+        mutate: createWorkoutTemplate,
+        error,
+        isPending,
+    } = useMutation({
+        mutationFn: async (workoutTemplateForm: IWorkoutTemplateFormState) => {
+            const dto = mapFromWorkoutTemplateFormToWorkoutTemplateRequest(workoutTemplateForm);
+            await workoutTemplatesApi.createWorkoutTemplate(dto);
+        },
+        onSuccess: async (response) => {
+            await queryClient.invalidateQueries({
+                queryKey: WorkoutTemplateQueryKeys.getAllWorkoutTemplates,
+            });
+            onSuccessCallback();
+        },
+        onError: onErrorCallback,
+    });
+
+    return { createWorkoutTemplate, isPending, error };
+}
+
+function mapFromWorkoutTemplateFormToWorkoutTemplateRequest(
+    workoutTemplateForm: IWorkoutTemplateFormState
+): WorkoutTemplateRequestDto {
+    return {
+        name: workoutTemplateForm.workoutTemplate.name,
+        exercises: workoutTemplateForm.workoutTemplate.exercises.map((exerciseId, index) => {
+            return {
+                exerciseId,
+                order: index + 1,
+                sets: workoutTemplateForm.exercises[exerciseId].sets.map((_, index) => {
+                    return {
+                        order: index + 1,
+                    };
+                }),
+            };
+        }),
+    };
 }
