b4fd978b79d581cb4b85674f332ada45cf2db0e4
Caleb Janhunen <calebjanhunen@gmail.com>
2024-12-29 20:47:15 -0500

    Update version for real

--- app.config.js
+++ app.config.js
@@ -14,7 +14,7 @@ export default {
         slug: 'fitracker',
         plugins: ['expo-router', 'expo-font'],
         scheme: 'fitracker',
-        version: '2.5.0',
+        version: '2.5.1',
         runtimeVersion: {
             policy: 'appVersion',
         },
