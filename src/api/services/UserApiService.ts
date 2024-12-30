2b7c3aa33d220cabfe2619b370941f417b5059cb
Caleb Janhunen <calebjanhunen@gmail.com>
2024-12-29 19:59:34 -0500

    Add call in service

--- src/api/services/UserApiService.ts
+++ src/api/services/UserApiService.ts
@@ -8,4 +8,8 @@ export const userApiService = {
         const response = await userApi.updateWeeklyWorkoutGoal(request);
         return response.data;
     },
+    async getCurrentUser(): Promise<UserProfileDto> {
+        const response = await userApi.getCurrentUser();
+        return response.data;
+    },
 };
