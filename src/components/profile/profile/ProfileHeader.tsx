299edc0508c7466dc5f364fb64ccd1729d56aff4
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-10 23:57:17 -0500

    Display level and progress on profile

--- src/components/profile/profile/ProfileHeader.tsx
+++ src/components/profile/profile/ProfileHeader.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import IonIcons from '@expo/vector-icons/Ionicons';
+import { useFocusEffect } from 'expo-router';
+import * as Progress from 'react-native-progress';
 import { UserProfileDto } from 'src/api/generated';
-import { Circle, H3, H5, View, XStack } from 'tamagui';
+import { Circle, H3, SizableText, useTheme, View } from 'tamagui';
 
 interface Props {
     user: UserProfileDto;
 }
 
 export default function ProfileHeader({ user }: Props) {
+    const [progress, setProgress] = useState(0);
+    const theme = useTheme();
+    useFocusEffect(
+        useCallback(() => {
+            setProgress(user.currentXp / user.xpNeededForNextLevel);
+
+            return () => {
+                setProgress(0);
+            };
+        }, [])
+    );
+
     return (
         <View alignItems='center' marginBottom='$space.5'>
-            <Circle size={100} backgroundColor='$gray8'>
-                <IonIcons name='person-outline' size={70} />
-            </Circle>
+            <View position='relative' alignItems='center' justifyContent='center'>
+                <Circle
+                    backgroundColor='$blue10'
+                    size={35}
+                    position='absolute'
+                    bottom={0}
+                    right={0}
+                    borderWidth={1}
+                    borderColor='$gray12'
+                    zIndex={1}
+                >
+                    <SizableText color='$gray1' fontWeight='bold' size='$6'>
+                        {user.level}
+                    </SizableText>
+                </Circle>
+                <Progress.Circle
+                    size={115}
+                    progress={progress}
+                    thickness={8}
+                    borderWidth={0}
+                    strokeCap='round'
+                    color={theme.blue10.val}
+                    unfilledColor={theme.background.val}
+                />
+                <Circle size={100} backgroundColor='$gray8' position='absolute'>
+                    <IonIcons name='person-outline' size={70} />
+                </Circle>
+            </View>
             <H3 paddingTop='$space.2'>
                 {user.firstName} {user.lastName}
             </H3>
-            <XStack gap='$space.2'>
-                <H5>XP:</H5>
-                <H5>{user.totalXp}</H5>
-            </XStack>
         </View>
     );
 }
