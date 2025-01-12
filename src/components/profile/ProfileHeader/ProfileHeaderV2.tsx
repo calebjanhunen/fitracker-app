38445d37f4483c7f580d3f41c0a4025d6bf41d24
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-12 14:51:59 -0500

    Add components to ProfileHeader component

--- src/components/profile/ProfileHeader/ProfileHeaderV2.tsx
+++ src/components/profile/ProfileHeader/ProfileHeaderV2.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-import IonIcons from '@expo/vector-icons/Ionicons';
+import { View, XStack, YStack } from 'tamagui';
+
 import { IUser } from 'src/redux/user/IUser';
-import { Circle, H3, H4, H5, H6, SizableText, View, XStack, YStack } from 'tamagui';
+import Avatar from './Avatar';
+import LevelDisplay from './LevelDisplay';
+import LevelProgressBar from './LevelProgressBar';
+import UserTitle from './UserTitle';
 
 interface Props {
     user: IUser;
@@ -12,16 +16,20 @@ export default function ProfileHeaderV2({ user }: Props) {
     return (
         <View marginBottom='$space.5'>
             <XStack gap='$space.5'>
-                <Circle size={100} backgroundColor='$gray8'>
-                    <IonIcons name='person-outline' size={70} />
-                </Circle>
-                <YStack gap='$space.0'>
-                    <H3>
-                        {user.firstName} {user.lastName}
-                    </H3>
-                    <SizableText marginTop='$space.-2' color='$gray10'>
-                        @{user.username}
-                    </SizableText>
+                <Avatar />
+                <YStack flex={1} gap='$space.3'>
+                    <XStack alignItems='center' justifyContent='space-between'>
+                        <UserTitle
+                            username={user.username}
+                            firstName={user.firstName}
+                            lastName={user.lastName}
+                        />
+                        <LevelDisplay level={user.level} />
+                    </XStack>
+                    <LevelProgressBar
+                        currentXp={user.currentXp}
+                        xpNeededForCurrentLevel={user.xpNeededForCurrentLevel}
+                    />
                 </YStack>
             </XStack>
         </View>
