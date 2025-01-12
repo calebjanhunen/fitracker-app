85720e38a8b659b2c405d2a4b411b61105c27ab9
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-12 13:06:07 -0500

    Add header right menu icon to profile screen

--- src/app/(app)/profile/_layout.tsx
+++ src/app/(app)/profile/_layout.tsx
@@ -1,10 +1,26 @@
-import { Stack } from 'expo-router';
+import IonIcons from '@expo/vector-icons/Ionicons';
+import { Link, Stack } from 'expo-router';
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { useTheme } from 'tamagui';
 
 export default function ProfileLayout() {
+    const theme = useTheme();
     return (
         <Stack>
-            <Stack.Screen name='index' />
+            <Stack.Screen
+                name='index'
+                options={{
+                    title: 'Profile',
+                    headerRight: () => (
+                        <Link href='/profile/ProfileSettings' asChild>
+                            <TouchableOpacity>
+                                <IonIcons color={theme.gray12.val} size={34} name='menu-outline' />
+                            </TouchableOpacity>
+                        </Link>
+                    ),
+                }}
+            />
             <Stack.Screen
                 name='[workoutId]'
                 options={{ headerShown: false, presentation: 'modal' }}
--- src/app/(app)/profile/index.tsx
+++ src/app/(app)/profile/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import IonIcons from '@expo/vector-icons/Ionicons';
-import { Link, Stack } from 'expo-router';
-import { TouchableOpacity } from 'react-native';
 import { useSelector } from 'react-redux';
 import { SizableText, useTheme, View } from 'tamagui';
 
@@ -21,18 +18,6 @@ export default function Profile() {
             paddingTop='$2'
             backgroundColor={theme.background.val}
         >
-            <Stack.Screen
-                options={{
-                    title: user.username,
-                    headerRight: () => (
-                        <Link href='/profile/ProfileSettings' asChild>
-                            <TouchableOpacity>
-                                <IonIcons color={theme.gray12.val} size={34} name='menu-outline' />
-                            </TouchableOpacity>
-                        </Link>
-                    ),
-                }}
-            />
             <ProfileHeader user={user} />
             <SizableText size='$5' fontWeight='bold' color='$blue10'>
                 Workout History
