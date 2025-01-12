7c197f86f1bc946a90ac447e894c2fdb89dfc45e
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-12 14:55:38 -0500

    Update LevelProgressBar to animate progress on screen focus

--- src/components/profile/ProfileHeader/LevelProgressBar.tsx
+++ src/components/profile/ProfileHeader/LevelProgressBar.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useFocusEffect } from 'expo-router';
+import React, { useCallback, useState } from 'react';
 
 import * as Progress from 'react-native-progress';
 import { SizableText, useTheme, YStack } from 'tamagui';
@@ -10,6 +11,18 @@ interface Props {
 
 export default function LevelProgressBar({ currentXp, xpNeededForCurrentLevel }: Props) {
     const theme = useTheme();
+    const [progress, setProgress] = useState(0);
+
+    // set progress on screen focus to animate the progress bar
+    useFocusEffect(
+        useCallback(() => {
+            setProgress(currentXp / xpNeededForCurrentLevel);
+
+            return () => {
+                setProgress(0);
+            };
+        }, [currentXp, xpNeededForCurrentLevel])
+    );
 
     return (
         <YStack>
@@ -19,7 +32,7 @@ export default function LevelProgressBar({ currentXp, xpNeededForCurrentLevel }:
             <Progress.Bar
                 borderRadius={10}
                 borderColor='transparent'
-                progress={0.3}
+                progress={progress}
                 height={13}
                 width={null}
                 unfilledColor={theme.gray6.val}
