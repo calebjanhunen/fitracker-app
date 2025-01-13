fca302e9dffa6ca6a84307c0a11fa480acd78497
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-13 16:25:46 -0500

    Increase gap between avatar and username in UserInfo component

--- src/components/leaderboard/UserLeaderboardCard/UserInfo.tsx
+++ src/components/leaderboard/UserLeaderboardCard/UserInfo.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function UserInfo({ username }: Props) {
     return (
-        <XStack alignItems='center' gap='$space.2'>
+        <XStack alignItems='center' gap='$space.4'>
             <UserAvatar size={40} />
             <SizableText size='$6' fontWeight='bold' color='$gray1'>
                 {username}
