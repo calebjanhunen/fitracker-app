e0d719237f7c2e53a5ff2f12203fb4fa4d389106
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-13 12:04:04 -0500

    Hide vertical scroll indicator in leaderboard FlatList

--- src/app/(app)/leaderboard/index.tsx
+++ src/app/(app)/leaderboard/index.tsx
@@ -10,6 +10,7 @@ export default function Leaderboard() {
         <View flex={1}>
             <H4 color='$blue10'>Total XP</H4>
             <FlatList
+                showsVerticalScrollIndicator={false}
                 contentContainerStyle={{ backgroundColor: theme.backgroundStrong.val }}
                 data={[0, 0, 0, 0, 0, 0, 0, 0]}
                 renderItem={({ item, index }) => <UserLeaderboardCard rank={index + 1} />}
