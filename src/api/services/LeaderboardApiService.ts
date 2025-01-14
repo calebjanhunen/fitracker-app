7c4d6c4061d8e1129ac9ffde3d6cd285f3c7c076
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-14 11:27:49 -0500

    Add fake timeout to getTotalXpLeaderboard for skeleton display

--- src/api/services/LeaderboardApiService.ts
+++ src/api/services/LeaderboardApiService.ts
@@ -5,6 +5,8 @@ const leaderboardApi = new LeaderboardApi(undefined, undefined, apiClient);
 
 export const leaderboardApiService = {
     async getTotalXpLeaderboard(): Promise<TotalXpLeaderboardUserDto[]> {
+        // Fake timeout to display sekeleton
+        await new Promise((resolve) => setTimeout(resolve, 700));
         const response = await leaderboardApi.getTotalXpLeaderboard();
         return response.data;
     },
