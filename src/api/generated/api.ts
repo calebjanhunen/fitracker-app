121925aab24282067ab29f9b48311e2edcfa84eb
Caleb Janhunen <calebjanhunen@gmail.com>
2025-01-13 22:26:59 -0500

    Update api

--- src/api/generated/api.ts
+++ src/api/generated/api.ts
@@ -2085,6 +2085,107 @@ export class ExercisesApi extends BaseAPI {
 
 
 /**
+ * LeaderboardApi - axios parameter creator
+ * @export
+ */
+export const LeaderboardApiAxiosParamCreator = function (configuration?: Configuration) {
+    return {
+        /**
+         * 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        getTotalXpLeaderboard: async (options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            const localVarPath = `/api/leaderboard/totalXp`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication access-token required
+            // http bearer authentication required
+            await setBearerAuthToObject(localVarHeaderParameter, configuration)
+
+
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+    }
+};
+
+/**
+ * LeaderboardApi - functional programming interface
+ * @export
+ */
+export const LeaderboardApiFp = function(configuration?: Configuration) {
+    const localVarAxiosParamCreator = LeaderboardApiAxiosParamCreator(configuration)
+    return {
+        /**
+         * 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async getTotalXpLeaderboard(options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<Array<object>>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.getTotalXpLeaderboard(options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['LeaderboardApi.getTotalXpLeaderboard']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+    }
+};
+
+/**
+ * LeaderboardApi - factory interface
+ * @export
+ */
+export const LeaderboardApiFactory = function (configuration?: Configuration, basePath?: string, axios?: AxiosInstance) {
+    const localVarFp = LeaderboardApiFp(configuration)
+    return {
+        /**
+         * 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        getTotalXpLeaderboard(options?: RawAxiosRequestConfig): AxiosPromise<Array<object>> {
+            return localVarFp.getTotalXpLeaderboard(options).then((request) => request(axios, basePath));
+        },
+    };
+};
+
+/**
+ * LeaderboardApi - object-oriented interface
+ * @export
+ * @class LeaderboardApi
+ * @extends {BaseAPI}
+ */
+export class LeaderboardApi extends BaseAPI {
+    /**
+     * 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof LeaderboardApi
+     */
+    public getTotalXpLeaderboard(options?: RawAxiosRequestConfig) {
+        return LeaderboardApiFp(this.configuration).getTotalXpLeaderboard(options).then((request) => request(this.axios, this.basePath));
+    }
+}
+
+
+
+/**
  * UsersApi - axios parameter creator
  * @export
  */
