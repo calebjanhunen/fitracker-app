3d28821c47b30e87aaa4ef8e2cc46d77802ed159
Caleb Janhunen <calebjanhunen@gmail.com>
2025-02-02 19:41:35 -0500

    Update filtered data hook

--- src/hooks/common/useFilteredData.ts
+++ src/hooks/common/useFilteredData.ts
@@ -1,18 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useMemo, useState } from 'react';
 
 export function useFilteredData<T>(initialData: T[] | undefined, fieldToFilterBy: keyof T) {
     const [searchQuery, setSearchQuery] = useState<string>('');
-    const [filteredData, setFilteredData] = useState<T[]>(initialData ?? []);
 
-    useEffect(() => {
-        if (searchQuery && initialData) {
-            setFilteredData(
-                initialData.filter((d) =>
-                    String(d[fieldToFilterBy]).toLowerCase().includes(searchQuery.toLowerCase())
-                )
-            );
+    const filteredData = useMemo(() => {
+        if (!searchQuery) {
+            return initialData;
         }
-    }, [searchQuery]);
+
+        return initialData?.filter((d) =>
+            String(d[fieldToFilterBy]).toLowerCase().includes(searchQuery.toLowerCase())
+        );
+    }, [searchQuery, initialData]);
 
     return { searchQuery, setSearchQuery, filteredData };
 }
