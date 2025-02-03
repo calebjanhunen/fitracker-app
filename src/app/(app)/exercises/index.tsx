98ecbb5e6c9c574aa61902190856a7b0eff9dbe1
Caleb Janhunen <calebjanhunen@gmail.com>
2025-02-02 21:51:44 -0500

    Add body part and equipment filters

--- src/app/(app)/exercises/index.tsx
+++ src/app/(app)/exercises/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useGetAllExercises } from 'src/api/hooks';
+import { useGetAllExercises, useGetEquipmentAndBodyParts } from 'src/api/hooks';
+import DropdownMenu from 'src/components/common/DropdownMenu';
 import KeyboardAvoidingView from 'src/components/common/keyboard-avoiding-view';
 import { ExerciseList } from 'src/components/exercises';
-import { useFilteredData } from 'src/hooks/common/useFilteredData';
-import { H3, Input, useTheme, YStack } from 'tamagui';
+import { useFilteredExercises } from 'src/hooks/common/useFilteredData';
+import { H3, Input, useTheme, XStack, YStack } from 'tamagui';
 
 export default function ExercisesHome() {
+    const { equipment, bodyParts } = useGetEquipmentAndBodyParts();
     const theme = useTheme();
+    const [selectedBodyPart, setSelectedBodyParts] = useState('');
+    const [selectedEquipment, setSelectedEquipment] = useState('');
     const { data: exercises, isLoading, error } = useGetAllExercises();
     const {
         searchQuery,
         setSearchQuery,
         filteredData: filteredExercises,
-    } = useFilteredData(exercises, 'name');
+    } = useFilteredExercises(
+        exercises,
+        bodyParts.find((bp) => bp.id === Number(selectedBodyPart))?.name,
+        equipment.find((eq) => eq.id === Number(selectedEquipment))?.name
+    );
 
     return (
         <SafeAreaView
@@ -22,16 +30,63 @@ export default function ExercisesHome() {
         >
             <KeyboardAvoidingView>
                 <H3 paddingBottom='$space.3'>Exercises</H3>
-                <YStack paddingBottom='$space.5'>
+                <YStack paddingBottom='$space.5' gap='$space.2'>
                     <Input
                         placeholder='Search for exercise'
                         value={searchQuery}
                         onChangeText={setSearchQuery}
                         autoCorrect={false}
                     />
+                    <XStack justifyContent='space-between'>
+                        <DropdownMenu
+                            selectedVal={selectedBodyPart}
+                            setSelectedVal={setSelectedBodyParts}
+                            options={bodyParts}
+                            placeholder='All Body Parts'
+                            label='Body Parts'
+                            iconAfter={false}
+                            width={175}
+                            selectedColor='$blue8'
+                        />
+                        <DropdownMenu
+                            selectedVal={selectedEquipment}
+                            setSelectedVal={setSelectedEquipment}
+                            options={equipment}
+                            placeholder='All Equipment'
+                            label='Equipment'
+                            iconAfter={false}
+                            width={175}
+                            selectedColor='$blue8'
+                        />
+                    </XStack>
                 </YStack>
                 <ExerciseList exercises={filteredExercises} isLoading={isLoading} error={error} />
             </KeyboardAvoidingView>
         </SafeAreaView>
     );
 }
+
+const items = [
+    { id: 1, name: 'Appleqwdqwdqwdoqjidowij' },
+    { id: 2, name: 'Pear' },
+    { id: 3, name: 'Blackberry' },
+    { id: 4, name: 'Peach' },
+    { id: 5, name: 'Apricot' },
+    { id: 6, name: 'Melon' },
+    { id: 7, name: 'Honeydew' },
+    { id: 8, name: 'Starfruit' },
+    { id: 9, name: 'Blueberry' },
+    { id: 10, name: 'Raspberry' },
+    { id: 11, name: 'Strawberry' },
+    { id: 12, name: 'Mango' },
+    { id: 13, name: 'Pineapple' },
+    { id: 14, name: 'Lime' },
+    { id: 15, name: 'Lemon' },
+    { id: 16, name: 'Coconut' },
+    { id: 17, name: 'Guava' },
+    { id: 18, name: 'Papaya' },
+    { id: 19, name: 'Orange' },
+    { id: 20, name: 'Grape' },
+    { id: 21, name: 'Jackfruit' },
+    { id: 22, name: 'Durian' },
+];
--- src/hooks/common/useFilteredData.ts
+++ src/hooks/common/useFilteredData.ts
@@ -1,17 +1,39 @@
 import { useMemo, useState } from 'react';
+import { ExerciseResponseDto } from 'src/api/generated';
 
-export function useFilteredData<T>(initialData: T[] | undefined, fieldToFilterBy: keyof T) {
+export function useFilteredExercises(
+    initialExercises: ExerciseResponseDto[] | undefined,
+    bodyPart: string | undefined,
+    equipment: string | undefined
+) {
     const [searchQuery, setSearchQuery] = useState<string>('');
+    let filteredData = initialExercises;
 
-    const filteredData = useMemo(() => {
-        if (!searchQuery) {
-            return initialData;
+    filteredData = useMemo(() => {
+        if (!bodyPart) {
+            return filteredData;
         }
 
-        return initialData?.filter((d) =>
-            String(d[fieldToFilterBy]).toLowerCase().includes(searchQuery.toLowerCase())
+        return filteredData?.filter((e) => e.bodyPart === bodyPart);
+    }, [bodyPart, filteredData]);
+
+    filteredData = useMemo(() => {
+        if (!equipment) {
+            return filteredData;
+        }
+
+        return filteredData?.filter((e) => e.equipment === equipment);
+    }, [equipment, filteredData]);
+
+    filteredData = useMemo(() => {
+        if (!searchQuery) {
+            return filteredData;
+        }
+
+        return filteredData?.filter((e) =>
+            e.name.toLowerCase().includes(searchQuery.toLowerCase())
         );
-    }, [searchQuery, initialData]);
+    }, [searchQuery, filteredData]);
 
     return { searchQuery, setSearchQuery, filteredData };
 }
