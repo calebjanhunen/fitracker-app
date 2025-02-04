199994e2a30926bae09c77fee40b98fa54b1fb2c
Caleb Janhunen <calebjanhunen@gmail.com>
2025-02-03 22:44:42 -0500

    Add tabs

--- src/components/exercises/ExerciseDetailsModal/ExerciseDetailsModalBody.tsx
+++ src/components/exercises/ExerciseDetailsModal/ExerciseDetailsModalBody.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IErrorResponse } from 'src/api/client';
 import { ExerciseDetailsDto } from 'src/api/generated';
-import { SizableText, Spinner, View } from 'tamagui';
+import { SizableText, Spinner, Tabs, View } from 'tamagui';
 
 interface Props {
     isLoading: boolean;
@@ -9,7 +9,12 @@ interface Props {
     exerciseDetails: ExerciseDetailsDto | undefined;
 }
 
+const INFO_TAB_NAME = 'info';
+const HISTORY_TAB_NAME = 'workoutHistory';
+
 export default function ExerciseDetailsModalBody({ isLoading, error, exerciseDetails }: Props) {
+    const [selectedTab, setSelectedTab] = useState('');
+
     if (isLoading) {
         return (
             <View flex={1} alignItems='center' justifyContent='center'>
@@ -30,5 +35,46 @@ export default function ExerciseDetailsModalBody({ isLoading, error, exerciseDet
         );
     }
 
-    return <SizableText>{exerciseDetails.bodyPart}</SizableText>;
+    console.log(selectedTab);
+
+    return (
+        <View flex={1} paddingTop='$space.3'>
+            <Tabs
+                defaultValue='tab1'
+                orientation='horizontal'
+                flexDirection='column'
+                value={selectedTab}
+                onValueChange={setSelectedTab}
+            >
+                <Tabs.List>
+                    <Tabs.Tab
+                        flex={1}
+                        value={INFO_TAB_NAME}
+                        backgroundColor={selectedTab === INFO_TAB_NAME ? '$blue8' : '$gray6'}
+                    >
+                        <SizableText
+                            fontFamily='$body'
+                            color={selectedTab === INFO_TAB_NAME ? '$gray1' : '$gray12'}
+                            fontWeight='bold'
+                        >
+                            Info
+                        </SizableText>
+                    </Tabs.Tab>
+                    <Tabs.Tab
+                        flex={1}
+                        value={HISTORY_TAB_NAME}
+                        backgroundColor={selectedTab === HISTORY_TAB_NAME ? '$blue8' : '$gray6'}
+                    >
+                        <SizableText
+                            fontFamily='$body'
+                            color={selectedTab === HISTORY_TAB_NAME ? '$gray1' : '$gray12'}
+                            fontWeight='bold'
+                        >
+                            Workout History
+                        </SizableText>
+                    </Tabs.Tab>
+                </Tabs.List>
+            </Tabs>
+        </View>
+    );
 }
