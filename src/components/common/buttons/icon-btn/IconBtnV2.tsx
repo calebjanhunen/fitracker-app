843ef6ad148fa244c0fd171f73771d7c8c327931
Caleb Janhunen <calebjanhunen@gmail.com>
2025-02-24 22:19:54 -0500

    Add disabled styles

--- src/components/common/buttons/icon-btn/IconBtnV2.tsx
+++ src/components/common/buttons/icon-btn/IconBtnV2.tsx
@@ -1,5 +1,6 @@
 import IonIcons from '@expo/vector-icons/Ionicons';
 import React, { ComponentProps } from 'react';
+import { useTheme } from 'tamagui';
 import { Button } from '../button';
 
 interface Props extends ComponentProps<typeof Button> {
@@ -8,10 +9,29 @@ interface Props extends ComponentProps<typeof Button> {
     iconSize?: number;
 }
 
-export default function IconBtnV2({ iconName, iconColor, iconSize, ...props }: Props) {
+export default function IconBtnV2({
+    iconName,
+    iconColor,
+    iconSize,
+    disabled,
+    backgroundColor,
+    ...props
+}: Props) {
+    const theme = useTheme();
     return (
-        <Button {...props} autoHeight paddingHorizontal={8} paddingVertical={3} borderRadius={10}>
-            <IonIcons name={iconName} size={iconSize} color={iconColor} />
+        <Button
+            {...props}
+            autoHeight
+            paddingHorizontal={8}
+            paddingVertical={3}
+            borderRadius={10}
+            backgroundColor={disabled ? theme.gray3.val : backgroundColor}
+        >
+            <IonIcons
+                name={iconName}
+                size={iconSize}
+                color={disabled ? theme.gray8.val : iconColor}
+            />
         </Button>
     );
 }
